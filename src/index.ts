1df43d6a024f6519b53e124474e8c93e462d9412
piyo <chick.p.1120@gmail.com>
2025-01-12 10:22:24 +0900

    Add /svg

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { Hono } from 'hono'
+import { svg } from './svg'
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.get('/svg', (c) => {
+  c.status(200);
+  c.header("Content-Type", "image/svg+xml");
+  return c.body(svg);
 })
 
 export default app
